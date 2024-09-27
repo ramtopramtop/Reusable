59d02358dd00748147127a9dce2759040180e37c
ramtopramtop <ramtopramtop@rambler.ru>
2024-09-27 16:32:28 +0300

    1-4 Task done

--- Exercises/4-range.js
+++ Exercises/4-range.js
@@ -1,5 +1,11 @@
 'use strict';
 
-const range = null;
-
+const range = (start, end) => {
+  if (end < start) return [];
+  const Arr = Array(end - start + 1);
+  for (let i = 0; i <= end - start; i++) {
+    Arr[i] = start + i;
+  }
+  return Arr;
+};
 module.exports = { range };
--- Exercises/5-range-odd.js
+++ Exercises/5-range-odd.js
@@ -1,5 +1,18 @@
 'use strict';
 
-const rangeOdd = null;
+const rangeOdd = (start, end) => {
+  if (end < start) return [];
+  const Arr = Array(end - start + 1);
+  if (Math.trunk(start / 2) * 2 == start) { let i = 0 }
+  else { let i = 1; }
+  //console.log(start);
+  //console.log(end);
+  //console.log(Arr);
+  for (let i = 0; i <= end - start; i=i+2) {
+    //  console.log(i);
+    Arr[i] = start + i;
+  }
+  return Arr;
+};
 
 module.exports = { rangeOdd };
