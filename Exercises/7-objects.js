591266be88f4a5122f94a9a0c1ffbab020f8586d
ramtopramtop <ramtopramtop@rambler.ru>
2024-09-30 12:51:24 +0300

    Task 7 done

--- Exercises/7-objects.js
+++ Exercises/7-objects.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const fn = null;
+const fn = () => {
+  const newObject = { name: 'Маша', };
+  let anotherObject = { name: 'Оля', };
+  newObject.name = 'Вася';
+  anotherObject.name = 'Петя';
+  const oneMoreObject = { name: 'Полкан', };
+  anotherObject = oneMoreObject;  
+};
 
 module.exports = { fn };
